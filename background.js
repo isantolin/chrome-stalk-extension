6f8a65ecd81d3a30b4325ba7983ec5789db349ff
Ignacio Santolin <isantolin@users.noreply.github.com>
2015-10-29 11:46:04 -0300

    Simplified code, warnings corrected

--- background.js
+++ background.js
@@ -9,38 +9,41 @@ chrome.storage.local.get('extension_status', function (result) {
 
     if (result_extension_status === 'On') {
         ruleapplier();
+
     }
 
     chrome.browserAction.setBadgeText({text: result_extension_status});
 
 });
 
-chrome.browserAction.onClicked.addListener(function (tab) {
+if (typeof chrome.browserAction !== "undefined") {
+    chrome.browserAction.onClicked.addListener(function (tab) {
 
-    chrome.storage.local.get('extension_status', function (result) {
+        chrome.storage.local.get('extension_status', function (result) {
 
-        result_extension_status = result.extension_status;
+            result_extension_status = result.extension_status;
 
-        if (result_extension_status === 'On') {
-            result_extension_status = 'Off';
-        } else {
-            result_extension_status = 'On';
-        }
+            if (result_extension_status === 'On') {
+                result_extension_status = 'Off';
+            } else {
+                result_extension_status = 'On';
+            }
 
-        chrome.storage.local.set({'extension_status': result_extension_status});
-        chrome.browserAction.setBadgeText({text: result_extension_status});
+            chrome.storage.local.set({'extension_status': result_extension_status});
+            chrome.browserAction.setBadgeText({text: result_extension_status});
 
-        chrome.windows.getCurrent(function (win) {
-            chrome.tabs.getAllInWindow(win.id, function (tabs) {
-                for (i in tabs) {
-                    chrome.tabs.reload(tabs[i].id);
+            chrome.windows.getCurrent(function (win) {
+                chrome.tabs.getAllInWindow(win.id, function (tabs) {
+                    for (i in tabs) {
+                        chrome.tabs.reload(tabs[i].id);
 
-                }
+                    }
+                });
             });
-        });
 
+        });
     });
-});
+}
 
 chrome.storage.onChanged.addListener(
         function (changes, namespace) {
